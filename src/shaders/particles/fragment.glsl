07e7064b60d48ce5beb74c70302dd15fa31728a8
alessandrobattiato <alessandroDev@outlook.it>
2024-05-18 12:41:28 +0200

    feat: rounded and cleaner particles implemented

--- src/shaders/particles/fragment.glsl
+++ src/shaders/particles/fragment.glsl
@@ -3,7 +3,11 @@ void main()
     vec2 uv = gl_PointCoord;
     float distanceToCenter = distance(uv, vec2(0.5)); // 0.5 because we want to get the center of the particle
 
-    gl_FragColor = vec4(vec3(distanceToCenter), 1.0);
+    if (distanceToCenter > 0.5) {
+        discard; // this prevents everything around the center of the particle from being rendered
+    }
+
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
