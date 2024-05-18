5d389629c02e7dd5e109c6199398f74091cd4874
alessandrobattiato <alessandroDev@outlook.it>
2024-05-18 12:35:49 +0200

    feat: distance to the center implemented

--- src/shaders/particles/fragment.glsl
+++ src/shaders/particles/fragment.glsl
@@ -1,6 +1,9 @@
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    vec2 uv = gl_PointCoord;
+    float distanceToCenter = distance(uv, vec2(0.5)); // 0.5 because we want to get the center of the particle
+
+    gl_FragColor = vec4(vec3(distanceToCenter), 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
