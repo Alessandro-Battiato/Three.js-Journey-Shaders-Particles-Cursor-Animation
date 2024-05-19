08c38dd7962e322e7235f96dbc581df8e871f040
alessandrobattiato <alessandroDev@outlook.it>
2024-05-19 17:02:35 +0200

    fix: fixed position

--- src/shaders/particles/vertex.glsl
+++ src/shaders/particles/vertex.glsl
@@ -24,7 +24,7 @@ void main()
     newPosition += displacement;
 
     // Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
